4bb6ff3eeb5ede13d592ab83064b1cce46475431
BenNguyen97 <thanhluan28297@gmail.com>
2025-03-05 23:48:20 +0700

    update code

--- main.tf
+++ main.tf
@@ -26,13 +26,25 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-# Subnet
-resource "aws_subnet" "main_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
+# Subnet 1
+resource "aws_subnet" "main_subnet_1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "Luan-Main-Subnet"
+    Name = "Luan-Main-Subnet-1"
+  }
+}
+
+# Subnet 2 
+resource "aws_subnet" "main_subnet_2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Luan-Main-Subnet-2"
   }
 }
 
@@ -55,12 +67,17 @@ resource "aws_route_table" "main_route_table" {
 }
 
 # Route Table Association
-resource "aws_route_table_association" "main_route_assoc" {
-  subnet_id      = aws_subnet.main_subnet.id
+resource "aws_route_table_association" "main_route_assoc_1" {
+  subnet_id      = aws_subnet.main_subnet_1.id
   route_table_id = aws_route_table.main_route_table.id
 }
 
-# Security Group for EKS Nodes
+resource "aws_route_table_association" "main_route_assoc_2" {
+  subnet_id      = aws_subnet.main_subnet_2.id
+  route_table_id = aws_route_table.main_route_table.id
+}
+
+#Security Group for EKS Nodes
 resource "aws_security_group" "eks_sg" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -92,7 +109,7 @@ resource "aws_security_group" "mongo_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
   }
 
   ingress {
@@ -119,40 +136,34 @@ resource "aws_security_group" "mongo_sg" {
 resource "aws_iam_role" "eks_role" {
   name = "Luan-EKS-Role"
 
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "eks.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 # IAM Role for EKS Nodes
 resource "aws_iam_role" "eks_node_role" {
   name = "Luan-EKS-Node-Role"
 
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })  
+  # Attach policies for EKS worker nodes
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ]
 }
 
 # EKS Cluster
@@ -161,39 +172,43 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.main_subnet.id]
+    subnet_ids = [aws_subnet.main_subnet_1.id, aws_subnet.main_subnet_2.id]
   }
+
+  depends_on = [aws_iam_role.eks_role]
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
 
 # EKS Node Group
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.main_subnet.id]
+  subnet_ids      = [aws_subnet.main_subnet_1.id, aws_subnet.main_subnet_2.id]
   instance_types  = ["t3.medium"]
+
   scaling_config {
     desired_size = 1
     min_size     = 1
-    max_size     = 3
+    max_size     = 2
   }
+
+  depends_on = [aws_iam_role.eks_node_role, aws_eks_cluster.eks_cluster]
 }
 
 # EC2 Instance for MongoDB
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main_subnet.id
+  subnet_id     = aws_subnet.main_subnet_1.id
   key_name      = var.key_name
   security_groups = [aws_security_group.mongo_sg.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo amazon-linux-extras enable corretto8
-    sudo yum install -y mongodb-org
-    sudo systemctl start mongod
-    sudo systemctl enable mongod
-  EOF
+  user_data = file("user_data/mongodb.sh")
 
   tags = {
     Name = "Luan-MongoDB-Instance"
